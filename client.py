d10ee152e4a73551db0e116e9b5b2d91ae129a99
Anantha <arn.ananthan1998@gmail.com>
2021-05-29 17:10:05 +0530

    first commit

---
+++ client.py
@@ -0,0 +1,22 @@
+import socketio
+
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print('connection established')
+
+@sio.event
+def my_message(data):
+    print('message', data)
+    sio.emit('my response', {'response': 'my response'})
+     
+  
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+sio.connect('http://localhost:5000')
+sio.emit('my_message', "hello from client") 
+sio.wait()
